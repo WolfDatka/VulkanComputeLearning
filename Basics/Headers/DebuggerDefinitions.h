00e53c5208e296da118027dc10791972f4470aac
WolfDatka <wolfdatka@gmail.com>
2024-10-16 17:49:39 +0200

    Added some easy debugging

---
+++ Basics/Headers/DebuggerDefinitions.h
@@ -0,0 +1,18 @@
+#ifndef DEBUG_DEFINITION_H
+#define DEBUG_DEFINITION_H
+
+// DEBUG_MSG
+#if defined(DEBUG) || defined(FAST_DEBUG)
+    #define DEBUG_COUT_FUNC(msg) std::cout << "[DEBUG_MSG]: " << msg << std::endl
+#else
+    #define DEBUG_COUT_FUNC(msg)
+#endif
+
+// DEBUG_ERR_MSG
+#if defined(DEBUG) || defined(FAST_DEBUG)
+    #define DEBUG_CERR_FUNC(msg) std::cerr << "[DEBUG_ERR_MSG]: " << msg << std::endl
+#else
+    #define DEBUG_CERR_FUNC(msg)
+#endif
+
+#endif
